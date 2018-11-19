1d6418377b977b04acfe88976cb9864c6d9acf02
Rosemary Wang <rosemarywang888@gmail.com>
2018-11-19 11:57:57 -0500

    Include updated version of gcloud SDK.

--- profiles/circleci_gcp_agent/controls/circleci_gcp_agent.rb
+++ profiles/circleci_gcp_agent/controls/circleci_gcp_agent.rb
@@ -6,7 +6,7 @@ control 'check gcloud sdk' do
     it { should exist }
   end
   describe command('gcloud version') do
-    its('stdout') { should include '217.0.0' }
+    its('stdout') { should include '225.0.0' }
   end
 end
 
