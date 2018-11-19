9ee9b5f844dab2acffe97b83fc8509b3c8a45d79
Rosemary Wang <rosemarywang888@gmail.com>
2018-11-19 13:39:31 -0500

    Update with build dependencies.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,13 @@
 FROM google/cloud-sdk:alpine
-RUN apk update \
-    && apk add bash \
+RUN apk update && apk upgrade \
+    && apk add --no-cache bash git openssl ca-certificates docker python3 \
     && gcloud components install kubectl
+
+RUN python3 -m ensurepip && \
+    rm -r /usr/lib/python*/ensurepip && \
+    pip3 install --upgrade pip setuptools && \
+    if [ ! -e /usr/bin/pip ]; then ln -s pip3 /usr/bin/pip ; fi && \
+    if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /usr/bin/python; fi && \
+    rm -r /root/.cache
+
+RUN pip install jinja2 invoke
--- profiles/circleci_gcp_agent/controls/circleci_gcp_agent.rb
+++ profiles/circleci_gcp_agent/controls/circleci_gcp_agent.rb
@@ -21,3 +21,39 @@ control 'check kubectl' do
     its('stdout') { should include '1.10.7' }
   end
 end
+
+control 'check git' do
+  impact 1.0
+  title 'confirm git installed'
+  desc 'confirm git installed'
+  describe command('git version') do
+    its('stdout') { should include '2.18.1' }
+  end
+end
+
+control 'check openssl' do
+  impact 1.0
+  title 'confirm openssl installed'
+  desc 'confirm openssl installed'
+  describe command('openssl version') do
+    its('stdout') { should include '1.0.2' }
+  end
+end
+
+control 'check python' do
+  impact 1.0
+  title 'confirm python installed'
+  desc 'confirm python installed'
+  describe command('python --version') do
+    its('stdout') { should include '3.7.0' }
+  end
+  describe command('pip --version') do
+    its('stdout') { should include '18.1' }
+  end
+  describe command('pip show jinja2') do
+    its('stdout') { should include '2.10' }
+  end
+  describe command('invoke --version') do
+    its('stdout') { should include '1.2.0' }
+  end
+end
